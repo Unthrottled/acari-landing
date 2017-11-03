b50ba277a2da29845f31b1816391620b39d22099
cyclic-reference <alex@cyclicreference.space>
2017-11-03 13:21:00 -0500

    do i need this?

--- web-service/src/main/java/io/acari/landing/LandingApplication.java
+++ web-service/src/main/java/io/acari/landing/LandingApplication.java
@@ -3,6 +3,7 @@ package io.acari.landing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LandingApplication {
 
