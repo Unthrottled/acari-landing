af6e31228f6b7c1b0e1218ad39f0ac012ff110c6
cyclic-reference <alex@cyclicreference.space>
2017-11-03 13:37:35 -0500

    Hello Werld

---
+++ web-service/src/main/java/io/acari/landing/LandingRestController.java
@@ -0,0 +1,15 @@
+package io.acari.landing;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class LandingRestController {
+
+    @GetMapping("/")
+    public Mono<String> fetchBase(){
+      return Mono.fromSupplier(()->"Hello Werld!\n");
+    }
+}
