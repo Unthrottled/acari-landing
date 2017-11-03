f1400083cce01f6e214dfb712e720e991b160047
cyclic-reference <alex@cyclicreference.space>
2017-11-03 15:01:01 -0500

    mongo client up

--- web-service/src/main/java/io/acari/landing/LandingApplication.java
+++ web-service/src/main/java/io/acari/landing/LandingApplication.java
@@ -2,9 +2,13 @@ package io.acari.landing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 
-@SpringBootApplication
+@EnableReactiveMongoRepositories
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class LandingApplication {
 
 	public static void main(String[] args) {
---
+++ web-service/src/main/java/io/acari/landing/MongoConfig.java
@@ -0,0 +1,36 @@
+package io.acari.landing;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+  private static final Logger LOGGER = LoggerFactory.getLogger(MongoConfig.class);
+  private final Environment environment;
+
+  @Autowired
+  public MongoConfig(Environment environment) {
+    this.environment = environment;
+  }
+
+
+  @Bean
+  @Override
+  public MongoClient reactiveMongoClient() {
+    String property = environment.getProperty("acari.mongo.connectionString", "localhost:27017");
+    LOGGER.info(property);
+    return MongoClients.create(property);
+  }
+
+  @Override
+  protected String getDatabaseName() {
+    return "landing";
+  }
+}
