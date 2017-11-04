31c4319816f28c4d51add49b54314e6e9af77e33
cyclic-reference <alexsimons9999@gmail.com>
2017-11-04 17:51:59 -0500

    thinking

--- web-service/src/main/java/io/acari/landing/LandingRestController.java
+++ web-service/src/main/java/io/acari/landing/LandingRestController.java
@@ -1,8 +1,13 @@
 package io.acari.landing;
 
 
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.Part;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -11,5 +16,15 @@ public class LandingRestController {
     @GetMapping("/")
     public Mono<String> fetchBase() {
         return Mono.just( "Hello Werld!\n");
+    }
+
+    @PostMapping(value = "save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    Mono<Boolean> createEntry(@RequestBody Flux<Part> partFlux){
+        partFlux.subscribe(part -> {
+            part.content().subscribe(dataBuffer -> {
+                part.name();
+            });
+        });
+        return Mono.just(true);
     }
 }
