2043435b330c12415fecbdf066455f8e611d9c62
cyclic-reference <alex@cyclicreference.space>
2017-11-05 17:41:24 -0600

    so this works

--- web-service/src/main/java/io/acari/landing/LandingRestController.java
+++ web-service/src/main/java/io/acari/landing/LandingRestController.java
@@ -5,8 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.multipart.FormFieldPart;
 import org.springframework.http.codec.multipart.Part;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -30,9 +33,17 @@ public class LandingRestController {
     return Mono.just("Hello Werld!\n");
   }
 
-  @PostMapping(value = "save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-  Mono<Boolean> createEntry(@RequestBody Flux<Part> partFlux) {
-    return imageHandler.saveImage(partFlux)
-        .all(Objects::nonNull);
+  @PostMapping(value = "save", consumes = {
+      MediaType.MULTIPART_FORM_DATA_VALUE,
+      MediaType.IMAGE_PNG_VALUE,
+      MediaType.IMAGE_JPEG_VALUE,
+      MediaType.IMAGE_GIF_VALUE,
+      MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+
+  })
+  Mono<Boolean> createEntry(@RequestPart MultipartFile file) {
+    return Mono.just(true);
+//    return imageHandler.saveImage(partFlux)
+//        .all(Objects::nonNull);
   }
 }
