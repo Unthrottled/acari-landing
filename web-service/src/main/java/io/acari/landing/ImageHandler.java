ac730534c0172bc5ac21f9cafedec10833234d91
cyclic-reference <alex@cyclicreference.space>
2017-11-04 21:44:14 -0500

    This looks better?

--- web-service/src/main/java/io/acari/landing/ImageHandler.java
+++ web-service/src/main/java/io/acari/landing/ImageHandler.java
@@ -4,15 +4,22 @@ import com.mongodb.reactivestreams.client.gridfs.GridFSBucket;
 import com.mongodb.reactivestreams.client.gridfs.helpers.AsyncStreamHelper;
 import org.bson.types.ObjectId;
 import org.reactivestreams.Publisher;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+@Component
 public class ImageHandler {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImageHandler.class);
     private final GridFSBucket gridFSBucket;
 
     @Autowired
@@ -20,11 +27,12 @@ public class ImageHandler {
         this.gridFSBucket = gridFSBucket;
     }
 
-    public Publisher<ObjectId> saveImage(File file) {
-        try {
-            return gridFSBucket.uploadFromStream(file.getName(), AsyncStreamHelper.toAsyncInputStream(new FileInputStream(file)));
-        } catch (FileNotFoundException e) {
-            return Mono.error(e);
-        }
+    public Flux<ObjectId> saveImage(Flux<Part> partFlux) {
+        return partFlux.flatMap(part -> {
+            String name = part.name();
+            return part.content()
+                .map(DataBuffer::asInputStream)
+                .flatMap(inputStream -> gridFSBucket.uploadFromStream(name, AsyncStreamHelper.toAsyncInputStream(inputStream)));
+        });
     }
 }
