4d2ebfe0888013b44adfee3e22916deced367bf7
cyclic-reference <alex@cyclicreference.space>
2017-11-05 13:00:14 -0600

    gonna see how this works

---
+++ web-content/src/app/upload/UploadFile.component.ts
@@ -0,0 +1,37 @@
+import {Component} from '@angular/core';
+import {Observable} from "rxjs/Observable";
+import {UploadFileService} from "./UploadFile.service";
+@Component({
+    selector: 'file-upload',
+    template: require('./UploadFile.component.htm')
+})
+export class UploadFileComponent {
+
+
+    constructor(private uploadService: UploadFileService) {
+    }
+
+    private _selectedFiles: any[];
+
+    selectFile(event: any): void {
+        this.selectedFiles = event.target.files;
+    }
+
+
+    get selectedFiles(): any[] {
+        return this._selectedFiles;
+    }
+
+    set selectedFiles(value: any[]) {
+        this._selectedFiles = value;
+    }
+
+    get isUploadable(): Observable<boolean>{
+        return Observable.of(this.selectedFiles.length > 0);
+    }
+
+    upload(): void {
+        // this.selectedFiles
+        //     .map()
+    }
+}
