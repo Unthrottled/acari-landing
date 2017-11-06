bbdc9c8a9cf1831225196d5ec9da3706cf3e576c
cyclic-reference <alex@cyclicreference.space>
2017-11-05 19:16:07 -0600

    model

---
+++ web-content/src/app/project/Project.model.ts
@@ -0,0 +1,7 @@
+export class Project {
+    selectedReach: Blob;
+    orderNumber: any;//todo: type dis
+    gradientColors: any;//todo: type dis
+    description: any; //todo: type dis
+    location: any;//todo type dis
+}
