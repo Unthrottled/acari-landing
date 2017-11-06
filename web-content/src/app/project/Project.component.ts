07c1607a86d7f61e7644bb1ecd062646101beec6
cyclic-reference <alex@cyclicreference.space>
2017-11-05 19:12:40 -0600

    its a start

---
+++ web-content/src/app/project/Project.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'acari-project',
+    template: require('./Project.component.htm')
+})
+export class UploadFileComponent {
+
+}
