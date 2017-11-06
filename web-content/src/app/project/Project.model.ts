4e11fbd071371fd6c4067a0776b6000943307fc9
cyclic-reference <alexsimons9999@gmail.com>
2017-11-06 06:45:07 -0600

    reach model

--- web-content/src/app/project/Project.model.ts
+++ web-content/src/app/project/Project.model.ts
@@ -2,9 +2,10 @@ import {Location} from "./Location.model";
 import {Description} from "./Description.model";
 import {Background} from "./Background.model";
 import {ProjecRank} from "./ProjectRank.model";
+import {Reach} from "./Reach.model";
 
 export class Project {
-    selectedReach: Blob;
+    selectedReach: Reach;
     rank: ProjecRank;
     background: Background;
     description: Description;
---
+++ web-content/src/app/project/Reach.model.ts
@@ -0,0 +1,26 @@
+export class Reach {
+    private _reachId: string;
+    private _rawFile: Blob;
+
+
+    constructor(reachId: string) {
+        this._reachId = reachId;
+    }
+
+
+    get reachId(): string {
+        return this._reachId;
+    }
+
+    set reachId(value: string) {
+        this._reachId = value;
+    }
+
+    get rawFile(): Blob {
+        return this._rawFile;
+    }
+
+    set rawFile(value: Blob) {
+        this._rawFile = value;
+    }
+}
