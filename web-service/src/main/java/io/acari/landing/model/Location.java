19318422f5f22b47f9854c4f1ed7ed549ef4f825
cyclic-reference <alex@cyclicreference.space>
2017-11-10 13:55:41 -0600

    location model

--- web-service/src/main/java/io/acari/landing/model/Location.java
+++ web-service/src/main/java/io/acari/landing/model/Location.java
@@ -1,4 +1,17 @@
 package io.acari.landing.model;
 
 public class Location {
+  private String url;
+
+  public Location(String url) {
+    this.url = url;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
 }
