52f0c46b6d8014f3d5c66b051b9aaeddde69788f
cyclic-reference <alex@cyclicreference.space>
2017-11-10 13:51:58 -0600

    rank model

--- web-service/src/main/java/io/acari/landing/model/ProjectRank.java
+++ web-service/src/main/java/io/acari/landing/model/ProjectRank.java
@@ -1,4 +1,17 @@
 package io.acari.landing.model;
 
 public class ProjectRank {
+  private String rank;
+
+  public ProjectRank(String rank) {
+    this.rank = rank;
+  }
+
+  public String getRank() {
+    return rank;
+  }
+
+  public void setRank(String rank) {
+    this.rank = rank;
+  }
 }
