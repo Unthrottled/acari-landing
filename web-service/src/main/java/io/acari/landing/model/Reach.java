147b27464d53e12a9f53d23757a8058b91491488
cyclic-reference <alex@cyclicreference.space>
2017-11-10 13:54:55 -0600

    Reach model

--- web-service/src/main/java/io/acari/landing/model/Reach.java
+++ web-service/src/main/java/io/acari/landing/model/Reach.java
@@ -1,4 +1,18 @@
 package io.acari.landing.model;
 
 public class Reach {
+
+  private String reachId;
+
+  public Reach(String reachId) {
+    this.reachId = reachId;
+  }
+
+  public String getReachId() {
+    return reachId;
+  }
+
+  public void setReachId(String reachId) {
+    this.reachId = reachId;
+  }
 }
