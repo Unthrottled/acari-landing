461462791d8644d7757a0071bbdf4dfd3c8cde30
cyclic-reference <alex@cyclicreference.space>
2017-11-10 13:53:42 -0600

    Description Model

--- web-service/src/main/java/io/acari/landing/model/Description.java
+++ web-service/src/main/java/io/acari/landing/model/Description.java
@@ -1,5 +1,27 @@
 package io.acari.landing.model;
 
 public class Description {
+    private String excerpt;
+    private String preachySpeechy;
 
+  public Description(String excerpt, String preachySpeechy) {
+    this.excerpt = excerpt;
+    this.preachySpeechy = preachySpeechy;
+  }
+
+  public String getExcerpt() {
+    return excerpt;
+  }
+
+  public void setExcerpt(String excerpt) {
+    this.excerpt = excerpt;
+  }
+
+  public String getPreachySpeechy() {
+    return preachySpeechy;
+  }
+
+  public void setPreachySpeechy(String preachySpeechy) {
+    this.preachySpeechy = preachySpeechy;
+  }
 }
