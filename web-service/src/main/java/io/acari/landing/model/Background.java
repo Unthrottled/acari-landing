ccce2032f2b481955a49eddef8c5549c104a497b
cyclic-reference <alex@cyclicreference.space>
2017-11-10 13:51:03 -0600

    background model

--- web-service/src/main/java/io/acari/landing/model/Background.java
+++ web-service/src/main/java/io/acari/landing/model/Background.java
@@ -1,4 +1,27 @@
 package io.acari.landing.model;
 
 public class Background {
+  private String backgroundStyle;
+  private String textColor;
+
+  public Background(String backgroundStyle, String textColor) {
+    this.backgroundStyle = backgroundStyle;
+    this.textColor = textColor;
+  }
+
+  public String getBackgroundStyle() {
+    return backgroundStyle;
+  }
+
+  public void setBackgroundStyle(String backgroundStyle) {
+    this.backgroundStyle = backgroundStyle;
+  }
+
+  public String getTextColor() {
+    return textColor;
+  }
+
+  public void setTextColor(String textColor) {
+    this.textColor = textColor;
+  }
 }
