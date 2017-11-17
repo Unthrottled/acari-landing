26beb2aafeef62b09212a2b6f45106452382e4bb
cyclic-reference <alexsimons9999@gmail.com>
2017-11-17 06:46:32 -0600

    dis

--- web-service/src/main/java/io/acari/landing/ImageHandler.java
+++ web-service/src/main/java/io/acari/landing/ImageHandler.java
@@ -1,16 +1,20 @@
 package io.acari.landing;
 
 import com.mongodb.reactivestreams.client.gridfs.GridFSBucket;
+import com.mongodb.reactivestreams.client.gridfs.GridFSDownloadStream;
 import com.mongodb.reactivestreams.client.gridfs.helpers.AsyncStreamHelper;
 import org.bson.types.ObjectId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 @Component
 public class ImageHandler {
@@ -25,11 +29,17 @@ public class ImageHandler {
   public Mono<String> saveImage(MultipartFile multipartFile) {
     String name = multipartFile.getOriginalFilename();
     try {
-      return Mono.from(gridFSBucket.uploadFromStream(name, AsyncStreamHelper.toAsyncInputStream(multipartFile.getInputStream())))
+      return Mono.from(gridFSBucket.uploadFromStream(name,
+              AsyncStreamHelper.toAsyncInputStream(multipartFile.getInputStream())))
           .map(ObjectId::toHexString);
     } catch (IOException e) {
       LOGGER.warn("Error saving image", e);
       return Mono.error(e);
     }
+  }
+
+  public Flux<DataBuffer> fetchImage(String imageId){
+    GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(new ObjectId(imageId));
+    return Flux.empty();
   }
 }
--- web-service/src/main/java/io/acari/landing/LandingRestController.java
+++ web-service/src/main/java/io/acari/landing/LandingRestController.java
@@ -6,6 +6,7 @@ import io.acari.landing.model.ResponseProject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,44 +16,52 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api")
 public class LandingRestController {
-  private static final Logger LOGGER = LoggerFactory.getLogger(LandingRestController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LandingRestController.class);
 
-  private final ImageHandler imageHandler;
-  private final AllProjectHandler allProjectHandler;
-  private final ProjectCreationHandler projectCreationHandler;
+    private final ImageHandler imageHandler;
+    private final AllProjectHandler allProjectHandler;
+    private final ProjectCreationHandler projectCreationHandler;
 
-  @Autowired
-  public LandingRestController(ImageHandler imageHandler, AllProjectHandler allProjectHandler, ProjectCreationHandler projectCreationHandler) {
-    this.imageHandler = imageHandler;
-    this.allProjectHandler = allProjectHandler;
-    this.projectCreationHandler = projectCreationHandler;
-  }
+    @Autowired
+    public LandingRestController(ImageHandler imageHandler, AllProjectHandler allProjectHandler, ProjectCreationHandler projectCreationHandler) {
+        this.imageHandler = imageHandler;
+        this.allProjectHandler = allProjectHandler;
+        this.projectCreationHandler = projectCreationHandler;
+    }
 
 
-  @GetMapping("")
-  public Mono<String> fetchBase() {
-    return Mono.just("Hello Werld!\n");
-  }
+    @GetMapping("")
+    public Mono<String> fetchBase() {
+        return Mono.just("Hello Werld!\n");
+    }
 
-  @PostMapping(value = "image/save", consumes = {
-      MediaType.MULTIPART_FORM_DATA_VALUE,
-      MediaType.IMAGE_PNG_VALUE,
-      MediaType.IMAGE_JPEG_VALUE,
-      MediaType.IMAGE_GIF_VALUE,
-      MediaType.APPLICATION_FORM_URLENCODED_VALUE,
+    @PostMapping(value = "image/save", consumes = {
+            MediaType.MULTIPART_FORM_DATA_VALUE,
+            MediaType.IMAGE_PNG_VALUE,
+            MediaType.IMAGE_JPEG_VALUE,
+            MediaType.IMAGE_GIF_VALUE,
+            MediaType.APPLICATION_FORM_URLENCODED_VALUE,
 
-  })
-  public Mono<String> saveImage(@RequestPart MultipartFile reach) {
-    return imageHandler.saveImage(reach);
-  }
+    })
+    public Mono<String> saveImage(@RequestPart MultipartFile reach) {
+        return imageHandler.saveImage(reach);
+    }
+
+    @RequestMapping(value = "image/get/{id}", produces = {MediaType.IMAGE_PNG_VALUE,
+            MediaType.IMAGE_JPEG_VALUE,
+            MediaType.IMAGE_GIF_VALUE})
+    public Flux<DataBuffer> fetchImage(@PathVariable("id") String id) {
+        return imageHandler.fetchImage(id);
+    }
+
 
     @PostMapping(value = "project/create", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Mono<ResponseProject> saveProject(@RequestBody BaseProject newProject){
-      return projectCreationHandler.create(Mono.just(newProject));
+    public Mono<ResponseProject> saveProject(@RequestBody BaseProject newProject) {
+        return projectCreationHandler.create(Mono.just(newProject));
     }
 
     @GetMapping(value = "projects", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Flux<ResponseProject> allProjects(){
-      return allProjectHandler.findAll();
+    public Flux<ResponseProject> allProjects() {
+        return allProjectHandler.findAll();
     }
 }
