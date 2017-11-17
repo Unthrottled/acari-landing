d1429c7924d5d97a71339ab845f7c23b82432948
cyclic-reference <alex@cyclicreference.space>
2017-11-17 09:02:18 -0600

    so It does not like dis

--- web-service/src/main/java/io/acari/landing/ImageHandler.java
+++ web-service/src/main/java/io/acari/landing/ImageHandler.java
@@ -4,17 +4,20 @@ import com.mongodb.reactivestreams.client.gridfs.GridFSBucket;
 import com.mongodb.reactivestreams.client.gridfs.GridFSDownloadStream;
 import com.mongodb.reactivestreams.client.gridfs.helpers.AsyncStreamHelper;
 import org.bson.types.ObjectId;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.IOException;
-import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 @Component
 public class ImageHandler {
@@ -30,7 +33,7 @@ public class ImageHandler {
     String name = multipartFile.getOriginalFilename();
     try {
       return Mono.from(gridFSBucket.uploadFromStream(name,
-              AsyncStreamHelper.toAsyncInputStream(multipartFile.getInputStream())))
+          AsyncStreamHelper.toAsyncInputStream(multipartFile.getInputStream())))
           .map(ObjectId::toHexString);
     } catch (IOException e) {
       LOGGER.warn("Error saving image", e);
@@ -38,8 +41,25 @@ public class ImageHandler {
     }
   }
 
-  public Flux<DataBuffer> fetchImage(String imageId){
+  public Flux<DataBuffer> fetchImage(String imageId) {
     GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(new ObjectId(imageId));
-    return Flux.empty();
+    return Flux.generate(synchronousSink -> {
+      DefaultDataBufferFactory defaultDataBufferFactory = new DefaultDataBufferFactory();
+      ByteBuffer byteBuffer = ByteBuffer.allocate(4096);
+      Mono.from(gridFSDownloadStream.read(byteBuffer))
+          .subscribe(read->{
+            if (read < 0) {
+              synchronousSink.complete();
+              gridFSDownloadStream.close();
+            } else {
+              synchronousSink.next(defaultDataBufferFactory.wrap(byteBuffer));
+            }
+          }, throwable -> {
+            LOGGER.warn("Ohhhshit", throwable);
+            synchronousSink.complete();
+          }, ()->{
+            LOGGER.info("hmm");
+          });
+    });
   }
 }
