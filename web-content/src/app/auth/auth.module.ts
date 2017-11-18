b0a64d7b2b91f2f9df53e3ed6a3cb6e0941919be
cyclic-reference <alex@cyclicreference.space>
2017-11-18 16:14:14 -0600

    Auth Module

---
+++ web-content/src/app/auth/auth.module.ts
@@ -0,0 +1,14 @@
+
+import {NgModule} from "@angular/core";
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [],
+    bootstrap: [],
+    providers: [],
+    schemas: []
+})
+export class AuthModule {
+
+}
