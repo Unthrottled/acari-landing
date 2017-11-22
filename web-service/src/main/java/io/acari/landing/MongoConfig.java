da123988ede7ee40ab5ee2ece7b38ccbe9554d0f
cyclic-reference <alex@cyclicreference.space>
2017-11-22 08:10:34 -0600

    I will come back to revisit dis

--- web-service/src/main/java/io/acari/landing/MongoConfig.java
+++ web-service/src/main/java/io/acari/landing/MongoConfig.java
@@ -1,18 +1,9 @@
 package io.acari.landing;
 
-import com.google.common.collect.Lists;
-import com.mongodb.ConnectionString;
-import com.mongodb.async.client.MongoClientSettings;
-import com.mongodb.connection.ClusterConnectionMode;
-import com.mongodb.connection.ClusterSettings;
-import com.mongodb.connection.ServerSettings;
-import com.mongodb.connection.SslSettings;
-import com.mongodb.connection.netty.NettyStreamFactoryFactory;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.gridfs.GridFSBucket;
 import com.mongodb.reactivestreams.client.gridfs.GridFSBuckets;
-import com.mongodb.selector.ServerAddressSelector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +12,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 
-import javax.net.ssl.SSLContext;
-
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
   private static final Logger LOGGER = LoggerFactory.getLogger(MongoConfig.class);
   private final Environment environment;
-
 
   @Autowired
   public MongoConfig(Environment environment) {
@@ -39,21 +27,8 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
   @Override
   public MongoClient reactiveMongoClient() {
     String property = environment.getProperty("acari.mongo.connectionString", "localhost:27017");
-    ConnectionString connectionString = new ConnectionString(property);
-    return MongoClients.create(MongoClientSettings.builder()
-        .clusterSettings(ClusterSettings.builder()
-            .description("description")
-            .applyConnectionString(connectionString)
-            .build())
-        .sslSettings(SslSettings.builder()
-            .enabled(true)
-            .invalidHostNameAllowed(true)
-            .applyConnectionString(connectionString)
-            .build())
-        .streamFactoryFactory(NettyStreamFactoryFactory.builder().build())
-        .build());
+    return MongoClients.create(property);
   }
-
 
   @Override
   protected String getDatabaseName() {
