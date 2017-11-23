176579bafe639da86e1f6e43558be983fc8823df
cyclic-reference <alexsimons9999@gmail.com>
2017-11-23 08:35:46 -0600

    Parking

--- web-service/src/main/java/io/acari/landing/MongoConfig.java
+++ web-service/src/main/java/io/acari/landing/MongoConfig.java
@@ -1,9 +1,16 @@
 package io.acari.landing;
 
+import com.google.common.collect.Lists;
+import com.mongodb.ServerAddress;
+import com.mongodb.async.client.MongoClientSettings;
+import com.mongodb.connection.ClusterSettings;
+import com.mongodb.connection.SslSettings;
+import com.mongodb.connection.netty.NettyStreamFactoryFactory;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import com.mongodb.reactivestreams.client.gridfs.GridFSBucket;
 import com.mongodb.reactivestreams.client.gridfs.GridFSBuckets;
+import io.netty.channel.nio.NioEventLoopGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +18,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import javax.annotation.PreDestroy;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -22,12 +31,23 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     this.environment = environment;
   }
 
+  private final NioEventLoopGroup eventLoopGroup = new NioEventLoopGroup();
 
   @Bean
   @Override
   public MongoClient reactiveMongoClient() {
     String property = environment.getProperty("acari.mongo.connectionString", "localhost:27017");
-    return MongoClients.create(property);
+    return MongoClients.create(MongoClientSettings.builder()
+            .streamFactoryFactory(NettyStreamFactoryFactory.builder()
+                    .eventLoopGroup(eventLoopGroup)
+                    .build())
+            .sslSettings(SslSettings.builder()
+                    .enabled(true)
+                    .build())
+            .clusterSettings(ClusterSettings.builder()
+                    .hosts(Lists.newArrayList(new ServerAddress(property)))
+                    .build())
+            .build());
   }
 
   @Override
@@ -38,5 +58,10 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
   @Bean
   public GridFSBucket gridFsTemplate(MongoClient reactiveMongoClient) throws Exception {
     return GridFSBuckets.create(reactiveMongoClient.getDatabase(environment.getProperty("acari.mongo.landingDatabase", "landing")));
+  }
+
+  @PreDestroy
+  public void shutdown(){
+    eventLoopGroup.shutdownGracefully();
   }
 }
