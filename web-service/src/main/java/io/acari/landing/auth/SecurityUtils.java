f62699ddaa7cbf5c556dae790cc1fc96acc0968a
cyclic-reference <alex@cyclicreference.space>
2017-11-24 17:56:48 -0600

    less dead imports

--- web-service/src/main/java/io/acari/landing/auth/SecurityUtils.java
+++ web-service/src/main/java/io/acari/landing/auth/SecurityUtils.java
@@ -1,12 +1,7 @@
 package io.acari.landing.auth;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
-import java.util.Date;
-
 public class SecurityUtils {
-	public static final long EXPIRATION_TIME = 864_000_000; // 10 days
-	public static final String TOKEN_PREFIX = "Bearer ";
-	public static final String HEADER_STRING = "Authorization";
+  public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+  public static final String TOKEN_PREFIX = "Bearer ";
+  public static final String HEADER_STRING = "Authorization";
 }
