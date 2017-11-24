80b0b1a7b1e1e5aed59eb79636e932575a6441fe
cyclic-reference <alex@cyclicreference.space>
2017-11-24 17:56:32 -0600

    less dead code

--- web-service/src/main/java/io/acari/landing/auth/SecurityUtils.java
+++ web-service/src/main/java/io/acari/landing/auth/SecurityUtils.java
@@ -9,12 +9,4 @@ public class SecurityUtils {
 	public static final long EXPIRATION_TIME = 864_000_000; // 10 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
-
-	public static String generateToken(String username) {
-		return Jwts.builder()
-				.setSubject(username)
-				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-				.signWith(SignatureAlgorithm.HS512, AuthConfigs.Configs.SECRET.getValue().getBytes())
-				.compact();
-	}
 }
