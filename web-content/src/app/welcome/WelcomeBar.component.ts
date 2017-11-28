00004d1730aa3c8ca2bcf5cf2dd1433f235ae279
cyclic-reference <alex@cyclicreference.space>
2017-11-28 15:25:58 -0600

    gonna want dis

---
+++ web-content/src/app/welcome/WelcomeBar.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+    selector: 'welcome-bar',
+    template: require('./WelcomeBar.component.htm')
+})
+export class WelcomeBarComponent {
+
+    constructor() {
+    }
+
+
+}
