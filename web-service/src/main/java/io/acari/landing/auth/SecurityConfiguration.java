b843be5b0460af8991a9433d23069e4998aa66a1
DATBOI <alexsimons9999@gmail.com>
2017-12-25 07:12:28 -0600

    keeping the core

--- web-service/src/main/java/io/acari/landing/auth/SecurityConfiguration.java
+++ web-service/src/main/java/io/acari/landing/auth/SecurityConfiguration.java
@@ -2,11 +2,6 @@ package io.acari.landing.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.core.userdetails.MapUserDetailsRepository;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsRepository;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
--- web-service/src/main/java/io/acari/landing/filter/JWTForwarder.java
+++ web-service/src/main/java/io/acari/landing/filter/JWTForwarder.java
@@ -1,11 +1,20 @@
 package io.acari.landing.filter;
 
+import io.acari.landing.auth.AuthConfigs;
+import io.jsonwebtoken.Jwts;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static io.acari.landing.auth.SecurityUtils.HEADER_STRING;
+import static io.acari.landing.auth.SecurityUtils.TOKEN_PREFIX;
 
 @Component
 public class JWTForwarder implements WebFilter {
@@ -18,4 +27,38 @@ public class JWTForwarder implements WebFilter {
 
     return chain.filter(exchange);
   }
+
+//  protected void doFilterInternal(HttpServletRequest req,
+//                                  HttpServletResponse res,
+//                                  FilterChain chain) throws IOException, ServletException {
+//    String header = req.getHeader(HEADER_STRING);
+//
+//    if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+//      res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
+//      return;
+//    }
+//
+//    UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+//
+//    SecurityContextHolder.getContext().setAuthentication(authentication);
+//    chain.doFilter(req, res);
+//  }
+//
+//  private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+//    String token = request.getHeader(HEADER_STRING);
+//    if (token != null) {
+//      // parse the token.
+//      String user = Jwts.parser()
+//              .setSigningKey(AuthConfigs.Configs.SECRET.getValue().getBytes())
+//              .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+//              .getBody()
+//              .getSubject();
+//
+//      if (user != null) {
+//        return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+//      }
+//      return null;
+//    }
+//    return null;
+//  }
 }
