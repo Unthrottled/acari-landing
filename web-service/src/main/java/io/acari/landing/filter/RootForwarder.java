f5ce1e97bb861b90d67ab040ef493bf9673b4414
DATBOI <alexsimons9999@gmail.com>
2017-12-25 09:53:17 -0600

    say cheeze

--- web-service/src/main/java/io/acari/landing/filter/RootForwarder.java
+++ web-service/src/main/java/io/acari/landing/filter/RootForwarder.java
@@ -1,6 +1,9 @@
 package io.acari.landing.filter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
@@ -9,13 +12,13 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class RootForwarder implements WebFilter {
+  private static final Logger LOGGER = LoggerFactory.getLogger(RootForwarder.class);
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
     ServerHttpRequest request = exchange.getRequest();
-//    if (request.getURI().getPath().equals("/")) {
-//      return chain.filter(exchange.mutate().request(request.mutate().path("/index.html").build()).build());
-//    }
-
+    if (request.getURI().getPath().equals("/")) {
+      return chain.filter(exchange.mutate().request(request.mutate().path("/index.html").build()).build());
+    }
     return chain.filter(exchange);
   }
 }
