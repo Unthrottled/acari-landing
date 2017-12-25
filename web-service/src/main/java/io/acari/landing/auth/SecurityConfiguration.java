512970c779d82e4af3608ae4e2556835caf6481e
DATBOI <alexsimons9999@gmail.com>
2017-12-25 06:59:46 -0600

    hmm

--- web-service/src/main/java/io/acari/landing/auth/SecurityConfiguration.java
+++ web-service/src/main/java/io/acari/landing/auth/SecurityConfiguration.java
@@ -1,15 +1,24 @@
 package io.acari.landing.auth;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.core.userdetails.MapUserDetailsRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsRepository;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
+
+@Configuration
 public class SecurityConfiguration implements WebFluxConfigurer {
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
 
     }
-
 
 }
