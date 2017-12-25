73768411a08330b7b7ad72767593705ae782edb1
DATBOI <alexsimons9999@gmail.com>
2017-12-25 07:58:55 -0600

    This Looks sane

--- web-service/src/main/java/io/acari/landing/filter/JWTForwarder.java
+++ web-service/src/main/java/io/acari/landing/filter/JWTForwarder.java
@@ -1,64 +1,62 @@
 package io.acari.landing.filter;
 
+import com.google.common.base.Preconditions;
 import io.acari.landing.auth.AuthConfigs;
 import io.jsonwebtoken.Jwts;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
+import reactor.util.annotation.NonNull;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-import static io.acari.landing.auth.SecurityUtils.HEADER_STRING;
+import static io.acari.landing.auth.SecurityUtils.HEADER_KEY;
 import static io.acari.landing.auth.SecurityUtils.TOKEN_PREFIX;
 
 @Component
 public class JWTForwarder implements WebFilter {
-  @Override
-  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-    ServerHttpRequest request = exchange.getRequest();
-    if (request.getURI().getPath().equals("/")) {
-      return chain.filter(exchange.mutate().request(request.mutate().path("/index.html").build()).build());
+    @Override
+    @NonNull
+    public Mono<Void> filter(@NonNull ServerWebExchange exchange, @NonNull WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        List<String> authHeaders = request.getHeaders().get(HEADER_KEY);
+
+        if (authHeaders == null || authHeaders.stream().noneMatch(s -> s.startsWith(TOKEN_PREFIX))) {
+            //reject
+            response.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return response.setComplete();
+        }
+
+        return getAuthentication(authHeaders)
+                .defaultIfEmpty(null)//bad token
+                .map(token-> Preconditions.checkNotNull(token, "Unauthorized!"))
+                .flatMap(goodToken -> chain.filter(exchange));
+
     }
 
-    return chain.filter(exchange);
-  }
+    private Mono<UsernamePasswordAuthenticationToken> getAuthentication(List<String> request) {
+        return Mono.just(request)
+                .filter(Objects::nonNull)
+                .filter(headers -> !headers.isEmpty())
+                .map(headers -> headers.get(0))
+                .filter(Objects::nonNull)
+                .map(token ->
+                        Jwts.parser()
+                                .setSigningKey(AuthConfigs.Configs.SECRET.getValue().getBytes())
+                                .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+                                .getBody()
+                                .getSubject()
+                ).filter(Objects::nonNull)
+                .map(user -> new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>()));
 
-//  protected void doFilterInternal(HttpServletRequest req,
-//                                  HttpServletResponse res,
-//                                  FilterChain chain) throws IOException, ServletException {
-//    String header = req.getHeader(HEADER_STRING);
-//
-//    if (header == null || !header.startsWith(TOKEN_PREFIX)) {
-//      res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Access Denied");
-//      return;
-//    }
-//
-//    UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
-//
-//    SecurityContextHolder.getContext().setAuthentication(authentication);
-//    chain.doFilter(req, res);
-//  }
-//
-//  private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
-//    String token = request.getHeader(HEADER_STRING);
-//    if (token != null) {
-//      // parse the token.
-//      String user = Jwts.parser()
-//              .setSigningKey(AuthConfigs.Configs.SECRET.getValue().getBytes())
-//              .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
-//              .getBody()
-//              .getSubject();
-//
-//      if (user != null) {
-//        return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
-//      }
-//      return null;
-//    }
-//    return null;
-//  }
+    }
 }
